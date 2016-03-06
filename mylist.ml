f34e0f8f52d1b73f51e3ffff0eead40d26168e2d
kerebe_p <paul.kerebel@epitech.eu>
2016-03-06 19:59:42 +0100

    update

--- mylist.ml
+++ mylist.ml
@@ -26,15 +26,16 @@ let rec nth list x =
     failwith "nth : error"
 ;;
 
-(* let rec rev list = *)
-(*   match list with *)
-(*     | Item (elem, items) -> rev_append items list *)
-(*     | _ -> Item(elem, items) *)
-
 let rec append list1 list2 =
   match list1 with
   | Item(elem, items) -> Item (elem, append items list2)
   | _ -> list2
+;;
+
+let rec rev list =
+  match list with
+    | Item(elem, items) -> append (rev items) (Item(elem, Empty))
+    | _ -> Empty
 ;;
 
 let rev_append list1 list2 =
@@ -60,29 +61,63 @@ let rec map func list =
   | _ -> Empty
 ;;
 
-
-  (* let rec fold_left *)
+let rec fold_left f a list =
+  match list with
+  | Item(elem, items) -> (fold_left f (f a elem) items)
+  | _ -> a
+;;
 
 let rec for_all p list =
   match list with
-  | Item(elem, items) -> if (p elem) && (for_all p items) then true else false
-  | _ -> false
+  | Item(elem, items) -> (p elem) && (for_all p items)
+  | _ -> true
 ;;
 
 let rec exists p list =
   match list with
-  | Item(elem, items) -> if (p elem) || (for_all p items) then true else false
+  | Item(elem, items) -> if (p elem) then true else (exists p items)
   | _ -> false
 ;;
 
 let rec mem a list =
   match list with
-  | Item(elem, items) -> if (a = elem) || (mem a items) then true else false
+  | Item(elem, items) -> if (a = elem) then true else (mem a items)
   | _ -> false
 ;;
 
 let rec memq a list =
   match list with
-  | Item(elem, items) -> if (a == elem) || (memq a items) then true else false
+  | Item(elem, items) -> if (a == elem) then true else (memq a items)
   | _ -> false
 ;;
+
+let rec filter a list =
+  match list with
+  | Item(elem, items) -> if (a = elem) then Item(elem, filter a items) else (filter a items)
+  | _ -> Empty
+;;
+
+let rec mem_assoc a list =
+  match list with
+  | Item((elem1, elem2), items) -> if (elem1 = a) then true else (mem_assoc a items)
+  | _ -> false
+;;
+
+let rec assoc a list =
+  match list with
+  | Item((elem1, elem2), items) -> if (elem1 = a) then elem2 else (assoc a items)
+  | _ -> raise Not_found
+;;
+
+let rec split = function
+  | Item((elem1, elem2), items) -> begin match (split items) with
+                                   | (a, b) -> (Item(elem1, a), Item(elem2, b)) end
+  | _ -> (Empty, Empty)
+;;
+
+let rec remove_assoc a list =
+  match list with
+  | Item((elem1, elem2), items) -> if (elem1 = a) then items else (Item((elem1, elem2), remove_assoc a items))
+  | _ -> Empty
+;;
+
